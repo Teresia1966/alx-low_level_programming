5867e92589ac84ce54ddcaa29ca3842011dc3762
Teresia1966 <tessmacharia@gmail.com>
2023-08-11 16:31:22 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - is main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	      putchar(i);
+	putchar('\n');
+	return (0);
+}

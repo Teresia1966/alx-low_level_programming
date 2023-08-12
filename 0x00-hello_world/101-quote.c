6e84c9125541feeac1dd163fdfe8882a8dd53130
Teresia1966 <tessmacharia@gmail.com>
2023-08-12 13:29:04 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is the main function
+ *
+ * Return: is 1 when successful
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}

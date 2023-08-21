9a8eca3806cc60333f0809f3d83ed9c33fdb5a15
Teresia1966 <tessmacharia@gmail.com>
2023-08-21 12:13:07 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,6 +8,7 @@
 int _atoi(char *s)
 {
 	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1;
+	unsigned int i;
 
 	while (*(s + count) != '\0')
 	{
@@ -29,5 +30,4 @@ int _atoi(char *s)
 			m /= 10;
 		}
 		return (oi * pn);
-
 }

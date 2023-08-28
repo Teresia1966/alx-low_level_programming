99ec4fac64663832ba3e73bc06acd0ac6c97e065
Teresia1966 <tessmacharia@gmail.com>
2023-08-28 13:21:22 -0700

    task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
+void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
